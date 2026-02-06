1ad1b0bf4e80ba8caec65d6bff59cc90c7800e5a
mehul <mehulra2005@gmail.com>
2026-02-06 17:01:05 +0530

    fixed import error

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import mens from "../assets/images/Mens-ctg.png";
+import mens from "../assets/images/Mens-Ctg.png";
 import womens from "../assets/images/Women-ctg.png";
 import kids from "../assets/images/KIDS-ctg.png";
 const Category = () => {
   return (
-  
-      <div className="flex-container">
-              <div className="ctg-box">
-                <img src={mens} alt="img1" />
-              </div>
-              <div className="ctg-box">
-                <img src={womens} alt="img2" />
-                
-              </div>
-              <div className="ctg-box">
-                <img src={kids} alt="img3" />
-              </div>
-            </div>
+
+    <div className="flex-container">
+      <div className="ctg-box">
+        <img src={mens} alt="img1" />
+      </div>
+      <div className="ctg-box">
+        <img src={womens} alt="img2" />
+
+      </div>
+      <div className="ctg-box">
+        <img src={kids} alt="img3" />
+      </div>
+    </div>
   )
 }
 
